dbbb509d9027b47eeabf4f7582b5121a512113e5
Infynyty <infynytyy@gmail.com>
2023-03-08 20:16:59 +0100

    [WIP] work on A star

--- src/Pathfinding/AStar.c
+++ src/Pathfinding/AStar.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdint-gcc.h>
+#include <math.h>
 #include "Logger.h"
 #include "Position.h"
 
@@ -73,7 +73,6 @@ void sift_down(PriorityQueue *queue) {
 
 void insert_node(PriorityQueue *queue, BlockNode *node) {
     if (queue->size + 1 > queue->max_nodes) {
-        cmc_log(ERR, "Tried adding node to a priority queue that is already at max size.");
         exit(EXIT_FAILURE);
     }
     queue->size++;
@@ -116,10 +115,13 @@ typedef struct SearchArea {
     uint16_t z_length;
 } SearchArea;
 
-NodePosition *abs_pos_to_rel_pos(SearchArea *area, uint16_t index_one) {
-    uint16_t x_index = index_one % area->x_length;
-    uint16_t z_index = index_one % (area->x_length * area->z_length);
-    uint16_t y_index = index_one % (area->x_length * area->z_length * area->y_length);
+NodePosition *index_to_rel_pos(SearchArea *area, uint16_t index_one) {
+    uint16_t blocks_per_x = 1;
+    uint16_t blocks_per_z = area->x_length;
+    uint16_t blocks_per_y = area->x_length * area->y_length;
+    uint16_t x_index = (index_one / blocks_per_x) % area->x_length;
+    uint16_t z_index = (index_one / blocks_per_z) % area->z_length;
+    uint16_t y_index = (index_one / blocks_per_y) % area->y_length;
     NodePosition *position = malloc(sizeof(NodePosition));
     position->x = x_index;
     position->y = y_index;
@@ -128,10 +130,10 @@ NodePosition *abs_pos_to_rel_pos(SearchArea *area, uint16_t index_one) {
 }
 
 uint32_t abs_pos_to_index(SearchArea *area, Position *pos) {
-    int16_t x_delta = (int16_t) (pos->x - area->abs_pos_of_origin->x);
-    int16_t y_delta = (int16_t) (pos->y - area->abs_pos_of_origin->y);
-    int16_t z_delta = (int16_t) (pos->z - area->abs_pos_of_origin->z);
-    return x_delta + z_delta * area->z_length + y_delta * area->y_length;
+    int16_t x_delta = (int16_t) fabs((pos->x - area->abs_pos_of_origin->x));
+    int16_t y_delta = (int16_t) fabs((pos->y - area->abs_pos_of_origin->y));
+    int16_t z_delta = (int16_t) fabs((pos->z - area->abs_pos_of_origin->z));
+    return x_delta + z_delta * area->x_length + y_delta * area->x_length * area->z_length;
 }
 
 uint32_t get_heuristic(NodePosition *pos_1, NodePosition *pos_2) {
@@ -139,9 +141,9 @@ uint32_t get_heuristic(NodePosition *pos_1, NodePosition *pos_2) {
 }
 
 SearchArea *search_area_new(Position *pos_1, Position *pos_2) {
-    uint16_t x_delta = (uint16_t) (pos_1->x - pos_2->x);
-    uint16_t y_delta = (uint16_t) (pos_1->y - pos_2->y);
-    uint16_t z_delta = (uint16_t) (pos_1->z - pos_2->z);
+    uint16_t x_delta = (uint16_t) fabs((pos_1->x - pos_2->x));
+    uint16_t y_delta = (uint16_t) fabs((pos_1->y - pos_2->y));
+    uint16_t z_delta = (uint16_t) fabs((pos_1->z - pos_2->z));
 
     int16_t x_low = (int16_t) (pos_1->x < pos_2->x ? pos_1->x : pos_2->x);
     int16_t y_low = (int16_t) (pos_1->x < pos_2->y ? pos_1->y : pos_2->y);
@@ -156,11 +158,35 @@ SearchArea *search_area_new(Position *pos_1, Position *pos_2) {
     return area;
 }
 
+bool position_equals(BlockNode *pos1, BlockNode *pos2) {
+    return false;
+}
+
+void get_neighbours(BlockNode *node) {
+
+}
+
 Position** find_path(Position *start, Position *goal) {
     SearchArea *area = search_area_new(start, goal);
     PriorityQueue *queue = priority_queue_new(area->x_length * area->y_length * area->z_length);
-    uint16_t came_from[area->x_length * area->y_length * area->z_length];
-    uint16_t costs[area->x_length * area->y_length * area->z_length];
+    BlockNode *costs[area->x_length * area->y_length * area->z_length];
+    BlockNode *parents[area->x_length * area->y_length * area->z_length];
 
-    insert_node(queue, block_node_new(abs_pos_to_rel_pos()))
+    BlockNode *start_node = block_node_new(abs_pos_to_index(area, start), 0);
+    BlockNode *goal_node = block_node_new(abs_pos_to_index(area, goal), 0);
+    insert_node(queue, start_node);
+    costs[start_node->index] = 0;
+    parents[start_node->index] = start_node;
+
+    BlockNode *neighbours = malloc(sizeof(BlockNode) * 4);
+
+    while(queue->size != 0) {
+        BlockNode *current = extract_min(queue);
+
+        if (position_equals(current, goal_node)) {
+            break;
+        }
+
+        for (int i = 0; i < )
+    }
 }
